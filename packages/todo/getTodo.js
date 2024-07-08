963bff6a97ada1ec4f69c332f7599c110d0c02aa
Rachel <borapark13@gmail.com>
2024-07-08 22:45:50 +0900

    feat: todo 렌더링 구현

--- packages/todo/getTodo.js
+++ packages/todo/getTodo.js
@@ -5,28 +5,7 @@ const TODO_LIST = faker.helpers.arrayElement([
   { id: 2, title: "todo2", completed: false },
   { id: 3, title: "todo3", completed: false },
 ]);
-// todo list 만들기
-const getTodoElement = () => {
-  const ul = document.querySelector("ul.todo-list");
-  TODO_LIST.forEach((todo) => {
-    const li = document.createElement("li");
-    li.textContent = todo.title;
-    ul.appendChild(li);
-  });
-};
 
-// 완료되지 않은 todo 수 count 하기
-const getTodoCount = () => {
-  const count = TODO_LIST.filter((todo) => !todo.completed).length;
-  return count;
-};
-
-// selected 클래스를 추가한 필터 유형을 가진 링크
-const getFilterLink = () => {
-  const filterLinks = document.querySelectorAll("ul.filters");
-  filterLinks.forEach((link, index) => {
-    if (TODO_LIST[index].completed) {
-      link.classList.add("selected");
-    }
-  });
+export const getTodos = () => {
+  return TODO_LIST;
 };
--- packages/todo/view.js
+++ packages/todo/view.js
@@ -1,2 +1,48 @@
-//todo list 업데이트
-function getTodoElement(todo) {}
+// todo list 만들기
+const getTodoElement = (todo) => {
+  const { title, completed } = todo;
+  return `
+    <li ${completed ? "class='completed'" : ""}>
+      <div class="view">
+        <input type="checkbox" ${
+          completed ? "class='checked'" : ""
+        } class="toggle"/>
+        <label>${title}</label>
+        <button class="delete">x</button>
+      </div>
+      <input class="edit" value="${title}"/>
+    </li>
+  `;
+};
+
+// 완료되지 않은 todo 수 count 하기
+const getTodoCount = (todos) => {
+  const count = todos.filter((todo) => !todo.completed).length;
+  return `
+  ${count} Items Left.
+  `;
+};
+
+// selected 클래스를 추가한 필터 유형을 가진 링크
+export default (targetElement, state) => {
+  const { currentFilter, todos } = state;
+
+  const $el = targetElement.cloneNode(true);
+
+  const $list = $el.querySelector(".todo-list");
+  const $count = $el.querySelector(".todo-count");
+  const $filters = $el.querySelector(".filters");
+
+  $list.innerHTML = todos.map(getTodoElement).join("");
+  $count.textContent = getTodoCount(todos);
+
+  Array.from($filters.querySelectorAll("li a")).forEach((a) => {
+    if (a.textContent === currentFilter) {
+      a.classList.add("selected");
+    } else {
+      a.classList.remove("selected");
+    }
+  });
+
+  return $el;
+};
