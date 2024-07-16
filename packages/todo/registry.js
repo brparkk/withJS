d10c75b74553acfca7f48b693f452324355c3329
Rachel <borapark13@gmail.com>
2024-07-16 23:33:57 +0900

    comment: 주석 추가

--- packages/todo/registry.js
+++ packages/todo/registry.js
@@ -1,5 +1,13 @@
 let registry = {};
 
+/**
+ * 최초 DOM 요소에서 렌더링을 시작하려면
+ * 어플리케이션 root를 렌더하는 메소드를 제공해야 한다. -> renderRoot
+ * root
+ * @param {*} root
+ * @param {*} state
+ * @returns
+ */
 const renderRoot = (root, state) => {
   const cloneElement = (root) => root.cloneNode(true);
   return renderWrapper(cloneElement)(root, state);
