01dd918c7fd0571ef51914ced2e00fa375ea0b43
Rachel <borapark13@gmail.com>
2024-07-16 23:34:10 +0900

    feat: 동적 데이터 렌더링

--- packages/todo/index.js
+++ packages/todo/index.js
@@ -1,5 +1,6 @@
 import getTodos from "./data/getTodo";
 import registry from "./registry";
+
 import todos from "./view/todos";
 import filters from "./view/filters";
 import counter from "./view/counter";
@@ -13,8 +14,16 @@ registry.add("todos", todos);
 registry.add("filters", filters);
 registry.add("counter", counter);
 
-window.requestAnimationFrame(() => {
+const render = window.requestAnimationFrame(() => {
+  console.count("render");
   const main = document.querySelector(".todoapp");
   const newMain = registry.renderRoot(main, state);
   main.replaceWith(newMain);
 });
+
+window.setInterval(() => {
+  state.todos = getTodos();
+  render();
+}, 5000);
+
+render();
